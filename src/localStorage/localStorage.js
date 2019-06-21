5245a4432662d1b030805e094b085112535ae745
nguyennhuhaitrieu <nguyennhuhaitrieu@gmail.com>
2019-06-21 13:25:17 +0700

    update code login

--- src/localStorage/localStorage.js
+++ src/localStorage/localStorage.js
@@ -2,10 +2,10 @@ export const LoginStore = {
   setData(USER_ID, data) {
     return localStorage.setItem(USER_ID, data);
   },
-  removeData(USER_ID,) {
+  removeData(USER_ID) {
     localStorage.removeItem(USER_ID);
   },
-  getData(USER_ID,) {
+  getData(USER_ID) {
     return localStorage.getItem(USER_ID);
   }
 }
